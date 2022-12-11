416a1499171a426e3c7273336de1b8f6b0f60965
razumolla <rajuahmed9344@gmail.com>
2022-12-11 12:52:27 +0600

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+function PageNotFound() {
+    return (
+        <div>
+            <h1>
+                404 page with all custom styling necessary
+            </h1>
+        </div>
+    );
+}
+
+export default PageNotFound;
